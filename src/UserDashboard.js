c80bdaa96cf1d56ee1678ecafdf8ffa7aceec900
Jay Mehta <jayjmehta251203@gmail.com>
2026-03-02 13:17:51 +0530

    Change 2 error solved

--- src/UserDashboard.js
+++ src/UserDashboard.js
@@ -1,4 +1,4 @@
-﻿import React, { useMemo, useState, useEffect } from 'react';
+﻿import React, { useMemo, useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DomeGallery from './DomeGallery';
 import { useAuth } from './contexts/AuthContext';
